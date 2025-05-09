e4afdfb4992d1a0d56b64004da1f81f0e456674f
blueberry1001 <kubarun317@gmail.com>
2025-05-09 13:03:04 +0900

    Change Font

--- main/src/consts.ts
+++ main/src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Toin Computer Association";
-export const SITE_DESCRIPTION = "TCA's Official Website";
+export const SITE_DESCRIPTION =
+  "筑波大学附属高校の部活、TCA（Toin Computer Association）の公式Webサイトです。過去の作品紹介や活動実績などを掲載しています。";
